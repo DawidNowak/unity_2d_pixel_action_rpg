d87a4b6c8fab93dda0b335728ace5a4c486bc07d
Dawid <nowak_dawid@hotmail.com>
2020-08-18 20:05:28 +0200

    monster_die animation, added to rabilion animator

---
+++ Assets/Scripts/Enemies/Rabilion.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class Rabilion : LivingObject
+{
+    private Animator animator;
+
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+        base.Start();
+    }
+
+    protected override void TakeDamage(int damage)
+    {
+        animator.SetTrigger(Consts.Hurt);
+        base.TakeDamage(damage);
+    }
+
+    protected override void Die()
+    {
+        animator.SetBool(Consts.Die, true);
+        base.Die();
+    }
+}
--- Assets/Scripts/Utils/Consts.cs
+++ Assets/Scripts/Utils/Consts.cs
@@ -4,8 +4,14 @@
     public static string Horizontal = nameof(Horizontal);
     public static string Vertical = nameof(Vertical);
 
+    //LIVINg OBJECT VARIABLES AND TRIGGERS
+    public static string Hurt = nameof(Hurt);                   //TODO: PO CHUJ TO MA TUTAJ BYĆ, PRZENIEŚĆ NA KLASKĘ ENEMY ALBO COŚ
+    public static string Die = nameof(Die);                     //TAK SAMO JAK SEKCJA PLAYER, WJEBAĆ TO BEZPOŚREDNIO DO PLAYERA
+
     //PLAYER ANIMATION VARIABLES AND TRIGGERS
     public static string MoveSpeed = nameof(MoveSpeed);
     public static string IsArmed = nameof(IsArmed);
     public static string Attack = nameof(Attack);
+
+
 }
