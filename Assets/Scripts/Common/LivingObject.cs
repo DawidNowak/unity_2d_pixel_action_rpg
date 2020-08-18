77e015b6e5cf90bb9edbb19222638bc6f6f7fdc9
Dawid <nowak_dawid@hotmail.com>
2020-08-18 19:50:27 +0200

    rabilion hurt animation, updated animator, base LivingObject class, made player, rabilion prefabs

---
+++ Assets/Scripts/Common/LivingObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public abstract class LivingObject : MonoBehaviour
+{
+    public int MaxHealth;
+    public int Health;
+
+    protected virtual void Start()
+    {
+        Health = MaxHealth;
+    }
+
+    protected virtual void TakeDamage(int damage)
+    {
+        Health -= damage;
+        if (Health <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected virtual void Die()
+    {
+        var collider = GetComponent<Collider2D>();
+        if (collider != null)
+        {
+            collider.enabled = false;
+        }
+
+        enabled = false;
+    }
+}
