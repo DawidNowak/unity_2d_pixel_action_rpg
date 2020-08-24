2f6b0a621011090107096f88f1f347c21635a624
Dawid <nowak_dawid@hotmail.com>
2020-08-24 20:40:21 +0200

    trees prefabs, initial forest map

---
+++ Assets/Scripts/GameManagement/SpriteRendererOrderSystem.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SpriteRendererOrderSystem : MonoBehaviour
+{
+    void Update()
+    {
+        SpriteRenderer[] renderers = FindObjectsOfType<SpriteRenderer>();
+
+        foreach (var renderer in renderers)
+        {
+            renderer.sortingOrder = (int)(renderer.transform.position.y * -10);
+        }
+    }
+}
