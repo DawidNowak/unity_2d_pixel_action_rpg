bfb82ca8cbf438e4b43a3863d82340e57f568c6a
Dawid <nowak_dawid@hotmail.com>
2020-09-03 18:12:11 +0200

    pause screen, Esc to pause

--- Assets/Scripts/GameManagement/GameManager.cs
+++ Assets/Scripts/GameManagement/GameManager.cs
@@ -4,9 +4,25 @@ public class GameManager : MonoBehaviour
 {
     private LevelSystem levelSystem;
 
+    public GameObject pauseScreen;
+
     void Awake()
     {
         levelSystem = new LevelSystem();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+        pauseScreen.SetActive(Time.timeScale == 0);
     }
 
     public LevelSystem GetLevelSystem()
