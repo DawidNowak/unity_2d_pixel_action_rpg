e13e38ae3ca90e5239fbc5fe4878170890abb198
Dawid <nowak_dawid@hotmail.com>
2020-09-15 05:32:59 +0200

    basic damage formula

---
+++ Assets/Scripts/Utils/DamageFormula.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class DamageFormula
+{
+    public static int CountDamage<T>(int baseDamage, CharacterStat<T> statistics) where T : StatModifier
+    {
+        return Mathf.RoundToInt(baseDamage * (1 + (statistics.CurrentValue / 10f)));
+    }
+}
